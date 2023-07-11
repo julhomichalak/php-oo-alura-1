d7b64a01ae0cf90e21981740305bf61f5288b191
Julho Michalak <julhomichalak@gmail.com>
2023-07-11 10:44:02 -0300

    version final

--- banco.php
+++ banco.php
@@ -2,7 +2,7 @@
 
 require_once('src/Conta.php');
 
-$primeiraConta = new Conta('056.945.649-52', "Julho Michalak");
+$primeiraConta = new Conta(new Titular('056.945.649-52', "Julho Michalak"));
 
 $primeiraConta->depositar(500);
 $primeiraConta->sacar(300);
